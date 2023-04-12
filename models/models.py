86991f5d62dfc66e2d2af206b9b692e89524b105
Juan Felipe Castaño <jf.castanol@uniandes.edu.co>
2023-04-12 11:26:34 -0500

    Model, Schema de Task

---
+++ models/models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import fields, Schema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+db = SQLAlchemy()
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    file_name = db.Column(db.String(128))
+    new_format = db.Column(db.String(128))
+    time_stamp = db.Column(db.DateTime, default= db.func.current_timestamp())
+    status = db.Column(db.String(128), default="uploaded")
+
+class TaskSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Task
+        load_instance = True
+    id = fields.String()
