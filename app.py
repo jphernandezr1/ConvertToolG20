93c34a992aa04c2b98846e3f85c1271a53ce2ef0
Juan Felipe Castaño <jf.castanol@uniandes.edu.co>
2023-04-12 22:54:07 -0500

    tablas creadas

--- app.py
+++ app.py
@@ -2,17 +2,24 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from views.views import TaskView, TasksView, db
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cloud-user:cloud-user@34.170.195.29:5432/cloud-converter-tool'
+
+db_pruebas = 'postgresql://postgres:12345@35.206.114.31:5432/grupo20_db'
+db_google = 'postgresql://cloud-user:cloud-user@34.170.195.29:5432/cloud-converter-tool'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_google
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app_context = app.app_context()
 app_context.push()
 
 db.init_app(app)
+migrate = Migrate(app, db)
+
 db.create_all()
 
 cors = CORS(app)
@@ -20,6 +27,7 @@ cors = CORS(app)
 api = Api(app)
 api.add_resource(TasksView, '/api/tasks')
 api.add_resource(TaskView, '/api/tasks/<int:id_task>')
+# api.add_resource(FileView, 'api/files/<str:file_name>')
 
 @app.route("/")
 def hello_world():
--- models/models.py
+++ models/models.py
@@ -5,14 +5,66 @@ from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 db = SQLAlchemy()
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key= True)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
     file_name = db.Column(db.String(128))
     new_format = db.Column(db.String(128))
-    time_stamp = db.Column(db.DateTime, default= db.func.current_timestamp())
+    time_stamp = db.Column(db.DateTime, default=db.func.current_timestamp())
     status = db.Column(db.String(128), default="uploaded")
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    nombre_archivo = db.Column(db.String(128))
+    ruta = db.Column(db.String(128))
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50))
+    password1 = db.Column(db.String(50))
+    password2 = db.Column(db.String(50))
+    email = db.Column(db.String(128))
+    tasks = db.relationship('Task', cascade='all, delete, delete-orphan')
+    files = db.relationship('File', cascade='all, delete, delete-orphan')
+
+
 
 class TaskSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Task
+        include_relationships = True
+        load_instance = True
+
+    id = fields.String()
+    file_name = fields.String()
+    new_format = fields.String()
+    time_stamp = fields.DateTime(dump_only=True)
+    status = fields.String()
+
+class FileSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = File
+        include_relationships = True
+        load_instance = True
+
+    id = fields.String()
+    nombre_archivo = fields.String()
+    ruta = fields.String()
+
+
+class UsuarioSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Usuario
+        include_relationships = True
         load_instance = True
     id = fields.String()
+    username = fields.String()
+    password1 = fields.String(load_only=True)
+    password2 = fields.String(load_only=True)
+    email = fields.String()
+    tasks = fields.Nested(TaskSchema, many=True)
+    files = fields.Nested(FileSchema, many=True)
+
+
+
+
+
